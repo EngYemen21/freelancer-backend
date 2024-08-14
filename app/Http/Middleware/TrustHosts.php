060b6b37cd2307cecf2e9e9c54bc93154c37ae4b
779475324 <m.bander.it@gmail.com>
2024-08-15 00:01:42 +0300

    update trust

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -11,6 +11,7 @@ class TrustHosts extends Middleware
      *
      * @return array<int, string|null>
      */
+    protected $proxies = '*';
     public function hosts(): array
     {
         return [
