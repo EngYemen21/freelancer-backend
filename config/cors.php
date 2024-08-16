4c36d5750027f4575397daa412c6521ccd413eb0
779475324 <m.bander.it@gmail.com>
2024-08-17 02:32:26 +0300

    update2

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://freelancer-lac-sigma.vercel.app')],
 
     'allowed_origins_patterns' => [],
 
